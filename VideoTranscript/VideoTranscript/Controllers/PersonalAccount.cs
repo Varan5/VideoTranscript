e4bdd386ff1d849a8527deb3331d67d451e3ea88
Varan5 <152421608+Varan5@users.noreply.github.com>
2025-09-28 14:42:09 +0300

    Добавил вкладку личный кабинет, забил сделать таски

---
+++ VideoTranscript/VideoTranscript/Controllers/PersonalAccount.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoTranscript.Controllers
+{
+    public class PersonalAccount : Controller
+    {
+        public IActionResult Account()
+        {
+            return View();
+        }
+    }
+}
